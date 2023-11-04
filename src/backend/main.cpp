af658f4d0620fe21fe210178ff9abab7561ce7a0
akash <akashanandscorpio@gmail.com>
2023-11-04 10:17:58 +0530

    Updated record class

---
+++ src/backend/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class record
+{
+    /* recordID, Date, Time, Amount, Category, PaymentMethod, Payee, Description, Deleted */
+    private:
+        int recordID;
+        // date
+        // time
+        double amount;
+        // category
+        // payment method
+        char payee[20];
+        char description[100];
+        int deleted;
+    public:
+        main(/* args */);
+        ~main();
+};
